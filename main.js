b9a727a708ffcf51f644e11409af5de2118cefec
John Maura <johntemwaura992@gmail.com>
2022-06-02 13:09:05 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+/*let length = document.getElementById("length").value;
+let width = document.getElementById("width").value;
+let getarea = document.getElementById("getarea");
+let areatext = document.getElementsByTagName("span");
+*/
+
+/*class Rectangle {
+    constructor (length, width){
+        this.length = length;
+        this.width = width;
+    }
+    area(){
+        return this.width * this.length;
+    }
+}
+getarea.addEventListener(onclick, Rectangle);
+console.log(length);
+console.log(width);
+console.log(length);
+
+
+let wth = 25;
+let lth = 4;
+
+class Rectangle{
+    constructor (wth, lth){
+        this.wth = wth;
+        this.lth = lth;
+    }
+    area(){
+        return this.width * this.length;
+    }
+}
+*/
